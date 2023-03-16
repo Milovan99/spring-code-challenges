6cd3e49a13580febfd55536edefd5821335ad3b3
Milovan99 <milovanjakovljevic78@gmail.com>
2023-03-16 13:33:11 +0100

    Schedule task with Spring Task

--- src/main/java/com/cecilireid/springchallenges/ScheduledTasks.java
+++ src/main/java/com/cecilireid/springchallenges/ScheduledTasks.java
@@ -2,13 +2,22 @@ package com.cecilireid.springchallenges;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ScheduledTasks {
+    @Autowired
+    CateringJobRepository cateringJobRepository;
     private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
+
+    @Scheduled(cron = "*/12 * * * * *")
     public void reportOrderStats() {
-        
+        List<CateringJob> jobs =cateringJobRepository.findAll();
+        logger.info("We have {} jobs",jobs.size());
     }
 }
